526cf36323a165e7c3199a53b8b51484e70a96fd
Dumka <71280190+Dumka-code@users.noreply.github.com>
2022-04-03 01:26:29 +0100

    Update Index.php

--- cohort-6.atwebpages.com/Index.php
+++ cohort-6.atwebpages.com/Index.php
@@ -1,1 +1,20 @@
 <!Doctype html>
+<html lang="en">
+  <!--This webpage is licensed under Cohort-6 copyright and created with fellow alx Cohort_6 Members read the Doc.html for more -->
+  <head>
+  
+  
+  <title>Cohort_6-lab</title>
+ 
+ </head>
+  
+  <html>
+  <body>
+  
+  <h1>Cohort_6 :  Hello World</h1>
+  
+  
+  
+  
+  </body>
+  </html>
